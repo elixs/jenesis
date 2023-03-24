a98c9d09089fe7643c120851679961493a6b5f6f
Elías Zelada <elixs.777@gmail.com>
2023-03-24 09:19:49 -0300

    player movement fix

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,17 +2,17 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -200.0
 const GRAVITY = 400
 const ACCELERATION = 1000
 
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity.y += GRAVITY 
+		velocity.y += GRAVITY * delta
 
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_VELOCITY * delta
+		velocity.y = JUMP_VELOCITY
 
 	var move_input = Input.get_axis("move_left", "move_right")
 	
