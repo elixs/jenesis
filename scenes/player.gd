795e13a8d0814ea1c666efa5f403b217319406fa
Belén <belen.vasquez.n@ug.uchile.cl>
2023-04-03 10:08:01 -0400

    cambios: pusimos patita que gira

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+
 const SPEED = 100.0
 const JUMP_VELOCITY = -150.0
 const GRAVITY = 400
@@ -20,59 +21,20 @@ var Enemy = preload("res://scenes/enemy.tscn")
 @onready var animation_tree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 
-func _ready():
-	animation_tree.active = true
-#	Engine.time_scale = 0.2
+
+@export var speed = 400
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = transform.x * Input.get_axis("down", "up") * speed
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-	
-	if is_on_floor():
-		current_airborne_time = 0
-
-	if Input.is_action_just_pressed("jump") and current_airborne_time < MAX_AIRBORNE_TIME:
-		jumping = true
-		current_jump_time = 0
-	
-	if jumping and current_jump_time <= MAX_JUMP_TIME:
-		velocity.y = JUMP_VELOCITY
-	
-	current_jump_time += delta
-	current_airborne_time += delta
-	
-	if Input.is_action_just_released("jump"):
-		jumping = false
-	
-
-	var move_input = Input.get_axis("move_left", "move_right")
-	
-	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION * delta)
-
+	get_input()
 	move_and_slide()
 
-	if Input.is_action_just_pressed("spawn"):
-		_spawn()
-	
-	# animation
-	
-	if is_on_floor():
-		if abs(velocity.x) > 10 or move_input:
-			playback.travel("run")
-		else:
-			playback.travel("idle")
-	else:
-		if velocity.y < 0:
-			playback.travel("jump")
-		else:
-			playback.travel("fall")
-	
-	if move_input:
-		pivot.scale.x = sign(move_input)
 
 
-func _spawn():
-	var enemy = Enemy.instantiate()
-	enemy.global_position = get_global_mouse_position()
-	enemy.rotation = (get_global_mouse_position() - global_position).angle() + PI / 2
-	get_parent().add_child(enemy)
+
+
+
+
